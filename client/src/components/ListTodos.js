c67b03592ea747d76ac5bc1dac9516f860d0a191
Nathan Gearke <n.gearke@gmail.com>
2021-04-18 16:18:12 -0500

    finished the table for todos, added a delete request and a filter for the UI

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,13 +1,30 @@
 import React, { Fragment, useEffect, useState } from 'react';
 
 const ListTodos = () => {
+    const [todos, setTodos] = useState([]); //set default as empty state
+
+    //delete todo function
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            });
+
+            setTodos(todos.filter(todo => todo.todo_id !== id)); //filters out todos with matching id
+        console.log(deleteTodo);
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
     const getTodos = async () => {
         try {
             const response = await fetch("http://localhost:5000/todos")
             //going to get data back, but need to parse it
             const jsonData = await response.json();
            
-            console.log(jsonData);
+            //console.log(jsonData);
+            setTodos(jsonData); //set the todos state = to the json data we fetched from the db
         } catch (err) {
             console.error(err.message);
         }
@@ -16,9 +33,10 @@ const ListTodos = () => {
     //makes a fetch request to restful API every time component is rendered
     useEffect(() => {
         getTodos();
-    })
+    }, []); //without the [], the useEffect would keep making requests over and over
     
-    
+    //console.log(todos);
+
     return <Fragment>
         <table className="table mt-5 text-center">
         <thead>
@@ -36,6 +54,16 @@ const ListTodos = () => {
             <td>john@example.com</td>
         </tr>
         */}
+        {todos.map(todo => (
+            <tr key={todo.todo_id}>
+                <td>{todo.description}</td>
+                <td> Edit </td>
+                <td> 
+                    <button className="btn btn-danger"
+                    onClick={() => deleteTodo(todo.todo_id)}> Delete </button>
+                </td>
+            </tr>
+        ))}
         </tbody>
     </table>
   </Fragment>
